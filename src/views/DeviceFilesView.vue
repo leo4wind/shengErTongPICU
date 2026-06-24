711b27fcd1ccf4c44377ef664332e71e2278a39f
小楼一夜听风 <27588068@qq.com>
2026-06-24 10:22:51 +0800

    fix: 图片路径添加 BASE_URL 前缀，修复 GitHub Pages 子路径下 404

--- src/views/DeviceFilesView.vue
+++ src/views/DeviceFilesView.vue
@@ -31,7 +31,7 @@ function jumpToField(fieldId: string) {
                 <el-tag effect="plain">{{ report.fileType }}</el-tag>
               </div>
               <div class="device-image-viewer" :class="{ missing: report.status === 'missing' }">
-                <img v-if="report.status !== 'missing'" :src="'/images/' + report.fileName" :alt="report.previewTitle" />
+                <img v-if="report.status !== 'missing'" :src="`${import.meta.env.BASE_URL}images/${report.fileName}`" :alt="report.previewTitle" />
                 <div v-else class="device-image-missing">
                   <span>未上传设备文件</span>
                 </div>
